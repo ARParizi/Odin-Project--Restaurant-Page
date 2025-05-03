8eec90be5bd8b4201d3bab7ba4d93d8687ce9ce1
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-05-03 23:46:22 +0330

    Style buttons, content div

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import './styles.css';
+//import './styles.css';
+import createHomeContentDiv from './home.js';
 
 console.log("Hello World from the Restaurant Page project.");
+console.log(document.querySelector('header').offsetHeight);
+
+const contentDiv = document.querySelector('#content');
+const homeContent = createHomeContentDiv();
+
+contentDiv.appendChild(homeContent);
